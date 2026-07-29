2a85c51dbe5dd247cbb5eb061bd6640a4f21a1e2
dbowersock18 <Dbowersock18@gmail.com>
2026-07-29 11:36:18 -0600

    added file structure for classes

--- CNEMS.cpp
+++ CNEMS.cpp
@@ -3,4 +3,5 @@ using namespace std;
 
 int main(void) {
     cout << "test";
+    // TODO: CREATE ALL THE CLASSES AND PUT IN A TEST STATEMENT. MAKE SURE THIS MAIN ENTRY POINT CONNECTS TO ALL CLASSES
 }
