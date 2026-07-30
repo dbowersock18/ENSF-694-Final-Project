b4e88b78edf1c35a2711b04e2ae58f3d70e66548
dbowersock18 <Dbowersock18@gmail.com>
2026-07-29 18:54:19 -0600

    little more work

--- Room.cpp
+++ Room.cpp
@@ -5,8 +5,14 @@ using namespace std;
 
 Room::Room(){
     this -> room_id = "Room - default ID";
+    this -> capacity = 0;
+    this -> room_type = "Room - default room type";
 }
 
 void Room::print_information(){
-    cout << "Information on the room: " << this -> room_id << endl;
+    cout << "Information on the room: " << endl; 
+    cout << "Room ID: " << this -> room_id << endl;
+    cout << "Room capacity " << this -> capacity << endl;
+    cout << "Room type: " << this -> room_type << endl;
+    cout << "End of information on room" << endl << endl;
 }
--- Room.h
+++ Room.h
@@ -5,6 +5,9 @@ using namespace std;
 class Room {
 private:
     string room_id;
+    int capacity;
+    string room_type;
+    // TODO: LIST OF BOOKING OBJECTS. WHAT ARE BOOKING OBJECTS?
 public:
     Room();
     string get_room_id();
